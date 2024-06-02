b615bd20227e65e3fa284926300df876e1676bd4
farhanadhyasta <farhanadhyasta24@gmail.com>
2024-06-02 14:49:57 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:update-expired-date')->daily();
+        $schedule->command('app:update-expired-date')->everyMinute();
     }
 
     /**
